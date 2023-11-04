fbe9e2fb627919702000ee34828bf164c0eac210
ZaharKaras <zahar.karas@gmail.com>
2023-11-05 02:33:46 +0300

    add entity

---
+++ Library/Library.Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Entities
+{
+    public class Book
+    {
+        [Key]
+        public string ISBN { get; set; }
+        public string? Name { get; set; }
+        public string? Genre { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+        public DateTime BorrowedTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+    }
+}
