88e08cc2e82f72222badc6e0d34806bd1f6dc69c
ZaharKaras <zahar.karas@gmail.com>
2023-11-12 03:43:24 +0300

    add profile for dto

---
+++ Library/Library.Api/MappingProfiles/BookMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Library.Api.DTOs;
+using Library.Domain.Entities;
+
+namespace Library.Api.MappingProfiles
+{
+    public class BookMappingProfile : Profile
+    {
+        public BookMappingProfile()
+        {
+            CreateMap<BookDto, Book>();
+        }
+    }
+}
