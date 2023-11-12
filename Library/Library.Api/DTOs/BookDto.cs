fd0f9469058dc441fc3f489747fb38e7cbb44249
ZaharKaras <zahar.karas@gmail.com>
2023-11-12 03:42:45 +0300

    add dto

---
+++ Library/Library.Api/DTOs/BookDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Api.DTOs
+{
+    public class BookDto
+    {
+        public string? ISBN { get; set; }
+        public string? Name { get; set; }
+        public string? Genre { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+    }
+}
